da3dabb73a30090423dbd02ab96b4293ab58ae60
Kai Perich <kperich@stud.hs-heilbronn.de>
2021-12-11 15:47:21 +0100

    Setup

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pandas as pd
 
-df = pd.read_csv('data.csv', sep=r'	')
+df_vgs = pd.read_csv('vgsales.csv')
+df_tgd = pd.read_csv('Twitch_game_data.csv')
 
-print(df.head())
-
+print(df_vgs.head())
+print(df_tgd.head())
