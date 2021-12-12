73e21316405c7c18c3e15116213d968086e20563
Kai Perich <kperich@stud.hs-heilbronn.de>
2021-12-12 14:14:13 +0100

    Create V3 CSV

--- main.py
+++ main.py
@@ -17,10 +17,14 @@ for index, row in df_vgs.iterrows():
     games_list.append(row['Name'])
 
 df_tgd_v2 = df_tgd
+df_tgd_v3 = df_tgd
+
 
 for index, row in df_tgd.iterrows():
     if row['Game'] not in games_list:
         df_tgd_v2 = df_tgd_v2.drop(index)
+    else:
+        df_tgd_v3 = df_tgd_v3.drop(index)
 
 df_tgd_v2.to_csv('empty.csv')
 
