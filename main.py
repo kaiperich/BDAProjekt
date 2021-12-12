250e3fd8b42812f1d7045dd0a147f7970847a70d
Kai Perich <kperich@stud.hs-heilbronn.de>
2021-12-12 13:29:11 +0100

    Drop NaNs

--- main.py
+++ main.py
@@ -5,3 +5,19 @@ df_tgd = pd.read_csv('Twitch_game_data.csv')
 
 print(df_vgs.head())
 print(df_tgd.head())
+
+print(df_vgs.shape)
+print(df_vgs.Rank.nunique())
+
+games_list = []
+
+df_tgd = df_tgd.dropna(subset=['Game'])
+
+for index, row in df_vgs.iterrows():
+    games_list.append(row['Name'])
+
+for index, row in df_tgd.iterrows():
+    if row['Game'] not in games_list:
+        df_tgd = df_tgd.drop(index)
+
+print(df_tgd['Game'].unique())
